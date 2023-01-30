015fc43346b19d2d095e94c0a4ac8004026d3a79
MaximKarlov <predator09071987@gmail.com>
2023-01-30 22:21:12 +0200

    job  is done on 50%

--- src/script.js
+++ src/script.js
@@ -1,39 +1,60 @@
 import API from './fetchCountries';
 
-const formUrl = document.querySelector('#form');
-const countryInfoUrl = document.querySelector('#country_Info');
-form.addEventListener('submit', onSubmit);
+const formUrl = document.querySelector('body');
+const formInputUrl = document.querySelector('#search-box');
+const countryListUrl = document.querySelector('.country-list')
+const countryInfoUrl = document.querySelector('.country-info');
+
+formInputUrl.addEventListener('change', onSubmit);
+// formInputUrl.setAttribute('name', 'input-country');
+
 
 function onSubmit(e) {
   e.preventDefault();
-  const inputValue = form.elements.news.value;
+  const inputValue =formInputUrl.value;
   const getCountry = API.fetchCountries(inputValue);
   console.log(getCountry);
   getCountry
     .then(data => {
-      const array = data[0];
-      createMarkup(array);
+      console.log(data.length);
+      if (data.length > 0) {
+        for (const key in data) {
+          console.log(data[key]);
+          createSearcher(data[key]);
+        }
+      } else {
+        const array = data[0];
+        createMarkupChoises(array);
+      }
+        
+
     })
-    .then(updateNewsList())
     .catch(onError)
-    .finally(() => form.reset());
+    // .finally(() => form.reset());
 }
 
-function createMarkup({ population, capital, languages, flags }) {
+function createMarkupChoises({ population, capital, languages, flags }) {
   let lang = [];
   console.log(languages);
   for (const key in languages) {
     lang.push(languages[key]);
   }
   return (countryInfoUrl.innerHTML = `<div class="article-card">
-  <div class="article-card-image"><img src="${flags.png}" width=80></div> 
+  <div class="article-card-image"><img src="${flags.svg}" width=80></div> 
         <h2 class="article-title">Capital: ${capital}</h2>
         <h3 class="article-author">Population: ${population}</h3>
         <p class="article-description">Languege: ${lang}</p>
     </div>`);
 }
 
-function updateNewsList(markup) {}
+function createSearcher({flags,name }) { 
+    return countryListUrl.insertAdjacentHTML("beforeend",`
+  <li class="item">
+      <img src="${flags.svg}" height=80> ${name.official}
+      </li> 
+`);
+  }
+
 
 function onError(err) {
   console.error(err);
