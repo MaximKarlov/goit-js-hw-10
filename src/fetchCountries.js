55263b6a8ff253e9a6598fd531ae001344d7f6ad
MaximKarlov <predator09071987@gmail.com>
2023-01-31 21:52:34 +0200

    allmost done

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,31 @@
+import Notiflix from 'notiflix';
+import Func from './components/functions';
 const ENDPOINT = 'https://restcountries.com/v3.1/name/';
 
 function fetchCountries(query) {
   return fetch(`${ENDPOINT}${query}`).then(res => res.json());
 }
 
-export default { fetchCountries };
+function perevirka(data) {
+  if (data.length == undefined) {
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+  }
+  if (data.length > 10)
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  else if (data.length >= 2 && data.length <= 10) {
+    for (const key in data) {
+      Func.createSearcher(data[key]);
+    }
+  }
+  if (data.length === 1) {
+    const array = data[0];
+    Func.createMarkupChoises(array);
+  }
+}
+
+export default {
+  fetchCountries,
+  perevirka,
+};
--- src/script.js
+++ src/script.js
@@ -1,64 +1,26 @@
+import './css/styles.css';
+import Func from './components/functions';
 import API from './fetchCountries';
 import Notiflix from 'notiflix';
 
+const DEBOUNCE_DELAY = 300;
+
 const formUrl = document.querySelector('body');
 const formInputUrl = document.querySelector('#search-box');
-const countryListUrl = document.querySelector('.country-list')
+const countryListUrl = document.querySelector('.country-list');
 const countryInfoUrl = document.querySelector('.country-info');
 
 formInputUrl.addEventListener('input', onSubmit);
 // formInputUrl.setAttribute('name', 'input-country');
 
-
 function onSubmit(e) {
   e.preventDefault();
-  const inputValue =formInputUrl.value;
+  const inputValue = formInputUrl.value;
   const getCountry = API.fetchCountries(inputValue);
-  console.log(getCountry);
   getCountry
     .then(data => {
-      console.log(data.length);
-      if (data.length > 10) Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-      else if (data.length >= 2 && data.length<=10) {
-        for (const key in data) {
-          console.log(data[key]);
-          createSearcher(data[key]);
-        }
-      } else {
-        const array = data[0];
-        createMarkupChoises(array);
-      }
-        
-
+      API.perevirka(data);
     })
-    .catch(onError)
-    // .finally(() => formUrl.reset());
-}
-
-function createMarkupChoises({ population, capital, languages, flags }) {
-  let lang = [];
-  console.log(languages);
-  for (const key in languages) {
-    lang.push(languages[key]);
-  }
-  return (countryInfoUrl.innerHTML = `<div class="article-card">
-  <div class="article-card-image"><img src="${flags.svg}" width=80></div> 
-        <h2 class="article-title">Capital: ${capital}</h2>
-        <h3 class="article-author">Population: ${population}</h3>
-        <p class="article-description">Languege: ${lang}</p>
-    </div>`);
-}
-
-function createSearcher({flags,name }) { 
-    return countryListUrl.insertAdjacentHTML("beforeend",`
-  <li class="item">
-      <img src="${flags.svg}" height=20>${name.official}
-      </li> 
-`);
-  }
-
-
-function onError(err) {
-  console.error(err);
-  updateNewsList('<p>Articles not found</p>');
+    .catch(Errors => Func.onError(Errors));
+  // .finally(() => formUrl.reset());
 }
