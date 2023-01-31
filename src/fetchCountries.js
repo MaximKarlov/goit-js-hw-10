8d275ca65647e53e7b7aa9f8fbecfb96ea602064
MaximKarlov <predator09071987@gmail.com>
2023-02-01 00:24:18 +0200

    finish work on HW10: add trim() & fix bugs))

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,31 +1,20 @@
-import Notiflix from 'notiflix';
-import Func from './components/functions';
+// const ENDPOINT = 'https://restcountries.com/v3.1/name/';
+
+// function fetchCountries(query) {
+//   return fetch(`${ENDPOINT}${query}`).then(res => res.json());
+// }
+
+// export default { fetchCountries };
+
+// -=============Variant 2===============
+
 const ENDPOINT = 'https://restcountries.com/v3.1/name/';
 
 function fetchCountries(query) {
-  return fetch(`${ENDPOINT}${query}`).then(res => res.json());
+  return fetch(
+    `${ENDPOINT}${query}?fields=name,population,capital,languages,flags`
+  ).then(res => res.json());
+  // .then(data => console.log(data));
 }
 
-function perevirka(data) {
-  if (data.length == undefined) {
-    Notiflix.Notify.failure('Oops, there is no country with that name');
-  }
-  if (data.length > 10)
-    Notiflix.Notify.info(
-      'Too many matches found. Please enter a more specific name.'
-    );
-  else if (data.length >= 2 && data.length <= 10) {
-    for (const key in data) {
-      Func.createSearcher(data[key]);
-    }
-  }
-  if (data.length === 1) {
-    const array = data[0];
-    Func.createMarkupChoises(array);
-  }
-}
-
-export default {
-  fetchCountries,
-  perevirka,
-};
+export default { fetchCountries };
--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
-import Func from './components/functions';
 import API from './fetchCountries';
 import Notiflix from 'notiflix';
+import _ from 'lodash';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -10,17 +10,68 @@ const formInputUrl = document.querySelector('#search-box');
 const countryListUrl = document.querySelector('.country-list');
 const countryInfoUrl = document.querySelector('.country-info');
 
-formInputUrl.addEventListener('input', onSubmit);
-// formInputUrl.setAttribute('name', 'input-country');
+formInputUrl.addEventListener('input', _.debounce(onSubmit, DEBOUNCE_DELAY));
 
 function onSubmit(e) {
   e.preventDefault();
-  const inputValue = formInputUrl.value;
+  const inputValue = formInputUrl.value.trim();
   const getCountry = API.fetchCountries(inputValue);
+  countryListUrl.innerHTML = ' ';
   getCountry
     .then(data => {
-      API.perevirka(data);
+      if (data.length < 1) {
+        countryListUrl.innerHTML = ' ';
+        countryInfoUrl.innerHTML = ' ';
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      }
+      if (data.length > 10) {
+        countryListUrl.innerHTML = ' ';
+        Notiflix.Notify.info(
+          `Too many matches found. Matches found: ${data.length} . Please enter a more specific name.`
+        );
+      }
+      if (data.length >= 2 && data.length <= 10) {
+        countryInfoUrl.innerHTML = ' ';
+        for (const key in data) {
+          createSearcher(data[key]);
+        }
+      } else {
+        if (data.length < 2) {
+          countryListUrl.innerHTML = ' ';
+          const array = data[0];
+          createMarkupChoises(array);
+        }
+      }
     })
-    .catch(Errors => Func.onError(Errors));
-  // .finally(() => formUrl.reset());
+    .catch(Errors => onError(Errors));
+}
+
+function createMarkupChoises({ name, population, capital, languages, flags }) {
+  let lang = [];
+  // console.log(languages);
+  for (const key in languages) {
+    lang.push(languages[key]);
+  }
+  return (countryInfoUrl.innerHTML = `<div class="country-card">
+  <div class="country-card-image"><img src="${flags.svg}" width=80">${name.common}</div> 
+        <h2 class="country-card-title" item="cap">Capital:<span> ${capital}</span></h2>
+        <h3 class="country-card-population">Population: <span>${population}</span></h3>
+        <h3 class="country-card-language">Languages:<span> ${lang}</span></h3>
+    </div>`);
+}
+
+function createSearcher({ flags, name }) {
+  return countryListUrl.insertAdjacentHTML(
+    'beforeend',
+    `
+  <li class="item">
+      <img src="${flags.svg}" class="country">${name.official}
+      </li> 
+`
+  );
+}
+
+function onError(Error) {
+  Notiflix.Notify.failure(Error.message);
+  // console.log(Error);
 }
